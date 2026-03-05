95a1e97600fda0294bc5b886ec02ec10840e7391
khairulfilhan <filhan.nusi@maybank.com>
2026-03-06 03:08:51 +0800

    Add ASSETS configuration for image resources

---
+++ src/config/assets.ts
@@ -0,0 +1,5 @@
+export const ASSETS = {
+  images: {
+    loginIllustration: 'https://img.freepik.com/premium-vector/successful-transaction-girl-smartphone-transferred-sent-money-coins-cash-electronic-account-internet-man-flat-vector-modern-illustration_566886-9670.jpg?w=2000',
+  }
+};
