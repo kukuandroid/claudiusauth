3e28397e00e47be58024903bb7cea14696fbc240
khairulfilhan <filhan.nusi@maybank.com>
2026-03-06 03:10:30 +0800

    Add registerIllustration asset to ASSETS configuration

--- src/config/assets.ts
+++ src/config/assets.ts
@@ -1,5 +1,6 @@
 export const ASSETS = {
   images: {
     loginIllustration: 'https://img.freepik.com/premium-vector/successful-transaction-girl-smartphone-transferred-sent-money-coins-cash-electronic-account-internet-man-flat-vector-modern-illustration_566886-9670.jpg?w=2000',
-  }
+    registerIllustration: 'https://img.freepik.com/premium-vector/smart-id-card-abstract-concept-vector-illustration_107173-25836.jpg?w=1480'
+}
 };
