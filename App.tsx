9ec9b55a1a245a93a0762f843197662a45a00c4d
khairulfilhan <filhan.nusi@maybank.com>
2026-03-06 03:51:32 +0800

    Remove Reactotron configuration file and update App.tsx to initialize Reactotron

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './src/utils/ReactotronConfig'; // Initialize Reactotron
 import { AuthProvider } from './src/context/AuthContext';
 import AppNavigator from './src/navigation/AppNavigator';
 
