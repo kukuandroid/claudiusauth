766329edfaf12fc747ac09970f7b381381fd37b5
khairulfilhan <filhan.nusi@maybank.com>
2026-03-06 10:19:00 +0800

    add AuthContextType interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,10 @@ export interface User {
 export interface StoredUser extends User {
   password: string;
 }
+
+export interface AuthContextType {
+  user: User | null;
+  login: (email: string, password: string) => Promise<void>;
+  signup: (name: string, email: string, password: string) => Promise<void>;
+  logout: () => void;
+}
