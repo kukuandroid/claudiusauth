e0019e6e41944ec592fa922c603c707179ef75a2
khairulfilhan <filhan.nusi@maybank.com>
2026-03-06 13:09:00 +0800

    add signup screen state management

--- src/screens/SignupScreen.tsx
+++ src/screens/SignupScreen.tsx
@@ -15,3 +15,13 @@ import { isValidEmail, isValidPassword, isNonEmpty } from '../utils/validators';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Signup'>;
+
+const SignupScreen: React.FC<Props> = ({ navigation }) => {
+  const { signup } = useAuth();
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [nameError, setNameError] = useState<string>('');
+  const [emailError, setEmailError] = useState<string>('');
+  const [passwordError, setPasswordError] = useState<string>('');
+  const [passwordVisible, setPasswordVisible] = useState<boolean>(false);
