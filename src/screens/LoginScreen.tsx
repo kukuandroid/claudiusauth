ae305b3a9a69bb48fb7d91c3f407da48a7266be4
khairulfilhan <filhan.nusi@maybank.com>
2026-03-06 12:00:00 +0800

    add login screen imports and types

---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  NativeSyntheticEvent,
+  TextInputSubmitEditingEventData,
+} from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../types';
+import { useAuth } from '../context/AuthContext';
+import { isValidEmail, isNonEmpty } from '../utils/validators';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Login'>;
